abbb2a99c801fd03750d5f0115d299627297bb00
tanmayshankar <tanmayshankar@github.com>
2020-01-03 14:29:06 -0500

    Update

--- Experiments/PolicyManagers.py
+++ Experiments/PolicyManagers.py
@@ -1325,7 +1325,7 @@ class PolicyManager_Pretrain(PolicyManager_BaseClass):
 				self.rollout_timesteps = len(sample_traj)
 				rollout_trajectory = self.rollout_robot_trajectory(sample_traj[0], latent_z)
 
-				distances[i] = ((sample_traj-self.rollout_trajectory)**2).mean()	
+				distances[i] = ((sample_traj-rollout_trajectory)**2).mean()	
 
 		self.mean_distance = distances[distances>0].mean()
 
