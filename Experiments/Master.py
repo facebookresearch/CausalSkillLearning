f148ed7f9c870e1925214362a02ad5ef507650db
tanmayshankar <tanmayshankar@github.com>
2020-01-07 00:15:47 -0500

    Update

--- Experiments/Master.py
+++ Experiments/Master.py
@@ -78,8 +78,10 @@ class Master():
 	def test(self):
 		if self.args.test_code:
 			self.tester = TestClass.MetaTestClass(self.args, self.policy_manager, self.dataset)
+	
+			embed()			
 			unittest.main(__name__, argv=['main'], exit=False)
-			
+
 def parse_arguments():
 	parser = argparse.ArgumentParser(description='Learning Skills from Demonstrations')
 
