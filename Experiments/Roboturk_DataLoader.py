2f37822be6241f3e10c063052ee9023e48285c06
tanmayshankar <tanmayshankar@github.com>
2020-01-08 23:56:17 -0500

    Adding code to preprocess dataset to compute object and robot states.

--- Experiments/Roboturk_DataLoader.py
+++ Experiments/Roboturk_DataLoader.py
@@ -94,6 +94,7 @@ class Roboturk_Dataset(Dataset):
 			data_element['is_valid'] = False
 			return data_element
 
+		# If we are here, the data element is presumably valid till now.
 		# Get joint angles from this state sequence.
 		joint_values = state_sequence[:,self.joint_angle_indices]
 		# Get gripper values from state sequence. 
@@ -174,6 +175,53 @@ class Roboturk_SegmentedDataset(Roboturk_Dataset):
         # gripper_open = [0.0115, -0.0115]
         # gripper_closed = [-0.020833, 0.020833]
 
+class Roboturk_PreprocessDataset(Roboturk_Dataset):
+
+	def __init__(self):
+
+		super(Roboturk_PreprocessDataset, self).__init__()
+		
+		self.dataset_directory = '/checkpoint/tanmayshankar/Roboturk/RoboTurkPilot'
+
+		# Require a task list. 
+		# The task name is needed for setting the environment, rendering. 
+		# We shouldn't need the environment for .. training though, should we? 
+
+		self.task_list = ["bins-Bread", "bins-Can", "bins-Cereal", "bins-Milk", "pegs-RoundNut", "pegs-SquareNut"]
+		self.num_demos = np.array([1069, 1069, 1069, 1069, 1144, 1145])
+		self.cummulative_num_demos = self.num_demos.cumsum()
+		self.cummulative_num_demos = np.insert(self.cummulative_num_demos,0,0)
+		# Append -1 to the start of cummulative_num_demos. This has two purposes. 
+		# The first is that when we are at index 0 of the dataset, if we appended 0, np.searchsorted returns 0, rather than 1. 
+		# For index 1, it returns 1. This was becoming inconsistent behavior for demonstrations in the same task. 
+		# Now with -1 added to cumm_num_demos, when we are at task index 0, it would add -1 to the demo index. This is necessary for ALL tasks, not just the first...  
+		# So that foils our really clever idea. 
+		# Well, if the searchsorted returns the index of the equalling element, it probably consistently does this irrespective of vlaue. 
+		# This means we can use this...
+
+		# No need for a clever solution, searchsorted has a "side" option that takes care of this. 
+
+		self.total_length = self.num_demos.sum()		
+
+		# Load data from all tasks. 			
+		self.files = []
+		for i in range(len(self.task_list)):
+			self.files.append(h5py.File("{0}/{1}/demo.hdf5".format(self.dataset_directory,self.task_list[i]),'r'))
+
+		# Seems to follow joint angles order:
+		# ('time','right_j0', 'head_pan', 'right_j1', 'right_j2', 'right_j3', 'right_j4', 'right_j5', 'right_j6', 'r_gripper_l_finger_joint', 'r_gripper_r_finger_joint', 'Milk0', 'Bread0', 'Cereal0', 'Can0').
+		# Extract these into... 
+		self.joint_angle_indices = [1,3,4,5,6,7,8]
+		self.gripper_indices = [9,10]	
+		self.ds_freq = 20
+		# self.r_gripper_r_finger_joint = np.array([-0.0116,   0.020833])
+		# self.r_gripper_l_finger_joint = np.array([-0.020833, 0.0135])
+
+		# [l,r]
+        # gripper_open = [0.0115, -0.0115]
+        # gripper_closed = [-0.020833, 0.020833]
+
+        embed()
 
 class Roboturk_Dataloader_Tester(unittest.TestCase):
 	
