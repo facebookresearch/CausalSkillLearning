495776e9671dbda7a51e988926fb9bd18dc94384
tanmayshankar <tanmayshankar@github.com>
2020-01-09 00:50:34 -0500

    Changing preproc

--- Experiments/Roboturk_DataLoader.py
+++ Experiments/Roboturk_DataLoader.py
@@ -175,56 +175,9 @@ class Roboturk_SegmentedDataset(Roboturk_Dataset):
 		# gripper_open = [0.0115, -0.0115]
 		# gripper_closed = [-0.020833, 0.020833]
 
-class Roboturk_PreprocessDataset(Roboturk_Dataset):
-
-	def __init__(self):
-
-		super(Roboturk_PreprocessDataset, self).__init__()
-		
-		self.dataset_directory = '/checkpoint/tanmayshankar/Roboturk/RoboTurkPilot'
-
-		# Require a task list. 
-		# The task name is needed for setting the environment, rendering. 
-		# We shouldn't need the environment for .. training though, should we? 
-
-		self.task_list = ["bins-Bread", "bins-Can", "bins-Cereal", "bins-Milk", "pegs-RoundNut", "pegs-SquareNut"]
-		self.num_demos = np.array([1069, 1069, 1069, 1069, 1144, 1145])
-		self.cummulative_num_demos = self.num_demos.cumsum()
-		self.cummulative_num_demos = np.insert(self.cummulative_num_demos,0,0)
-		# Append -1 to the start of cummulative_num_demos. This has two purposes. 
-		# The first is that when we are at index 0 of the dataset, if we appended 0, np.searchsorted returns 0, rather than 1. 
-		# For index 1, it returns 1. This was becoming inconsistent behavior for demonstrations in the same task. 
-		# Now with -1 added to cumm_num_demos, when we are at task index 0, it would add -1 to the demo index. This is necessary for ALL tasks, not just the first...  
-		# So that foils our really clever idea. 
-		# Well, if the searchsorted returns the index of the equalling element, it probably consistently does this irrespective of vlaue. 
-		# This means we can use this...
-
-		# No need for a clever solution, searchsorted has a "side" option that takes care of this. 
-
-		self.total_length = self.num_demos.sum()		
-
-		# Load data from all tasks. 			
-		self.files = []
-		for i in range(len(self.task_list)):
-			self.files.append(h5py.File("{0}/{1}/demo.hdf5".format(self.dataset_directory,self.task_list[i]),'r'))
-
-		# Seems to follow joint angles order:
-		# ('time','right_j0', 'head_pan', 'right_j1', 'right_j2', 'right_j3', 'right_j4', 'right_j5', 'right_j6', 'r_gripper_l_finger_joint', 'r_gripper_r_finger_joint', 'Milk0', 'Bread0', 'Cereal0', 'Can0').
-		# Extract these into... 
-		self.joint_angle_indices = [1,3,4,5,6,7,8]
-		self.gripper_indices = [9,10]	
-		self.ds_freq = 20
-		# self.r_gripper_r_finger_joint = np.array([-0.0116,   0.020833])
-		# self.r_gripper_l_finger_joint = np.array([-0.020833, 0.0135])
-
-		# [l,r]
-		# gripper_open = [0.0115, -0.0115]
-		# gripper_closed = [-0.020833, 0.020833]
-
 	def preprocess_dataset(self):
 
-		# for task_index in range(len(self.task_list)):
-		for task_index in range(1):
+		for task_index in range(len(self.task_list)):
 
 			print("#######################################")
 			print("Preprocessing task index: ", task_index)
@@ -244,37 +197,40 @@ class Roboturk_PreprocessDataset(Roboturk_Dataset):
 			task_demo_list = []
 
 			# For every element in the filelist of the element,
-			# for i in range(1,self.num_demos[task_index]+1):
-			for i in range(1,2):
+			for i in range(1,self.num_demos[task_index]+1):
 
 				print("Preprocessing task index: ", task_index, " Demo Index: ", i, " of: ", self.num_demos[task_index])
 			
 				# Create list of datapoints for this demonstrations. 
 				datapoint = {}
-				robot_state_list = []
-				object_state_list = []
 
 				# Get SEQUENCE of flattened states.
-				flattened_state_sequence = self.files[task_index]['data/demo_{0}/states'.format(i)].value
-				number_timesteps = flattened_state_sequence.shape[0]
-				robot_state_array = np.zeros((number_timesteps, robot_state_size))
-				object_state_array = np.zeros((number_timesteps, object_state_size))
+				try:
+					flattened_state_sequence = self.files[task_index]['data/demo_{0}/states'.format(i)].value
+					number_timesteps = flattened_state_sequence.shape[0]
+					robot_state_array = np.zeros((number_timesteps, robot_state_size))
+					object_state_array = np.zeros((number_timesteps, object_state_size))
 
-				# For every element in sequence, set environment state. 
-				for t in range(flattened_state_sequence.shape[0]):
+					# For every element in sequence, set environment state. 
+					for t in range(flattened_state_sequence.shape[0]):
 
-					self.env.sim.set_state_from_flattened(flattened_state_sequence[t])
+						self.env.sim.set_state_from_flattened(flattened_state_sequence[t])
 
-					# Now get observation. 
-					observation = self.env._get_observation()
+						# Now get observation. 
+						observation = self.env._get_observation()
 
-					# Robot and Object state appended to datapoint dictionary. 
-					robot_state_array[t] = observation['robot-state']
-					object_state_array[t] = observation['object-state']
+						# Robot and Object state appended to datapoint dictionary. 
+						robot_state_array[t] = observation['robot-state']
+						object_state_array[t] = observation['object-state']
 
+				except: 
+					
+					datapoint['robot_state_array'] = np.zeros((1, robot_state_size))
+					datapoint['object_state_array'] = np.zeros((1, object_state_size))
+					
 				# Put both lists in a dictionary. 
-				datapoint['robot_state_list'] = robot_state_array
-				datapoint['object_state_list'] = object_state_array
+				datapoint['robot_state_array'] = robot_state_array
+				datapoint['object_state_array'] = object_state_array
 
 				# Add this dictionary to the file_demo_list. 
 				task_demo_list.append(datapoint)
