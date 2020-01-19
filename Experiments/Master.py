bc3e776fa5265db3ed8a6a3f6a202439cd8a972f
tanmayshankar <tanmayshankar@github.com>
2020-01-18 23:24:36 -0500

    Updating RL things into single utils file

--- Experiments/Master.py
+++ Experiments/Master.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from headers import *
 import DataLoaders, MIME_DataLoader, Roboturk_DataLoader
-from PolicyManagers import PolicyManager_Joint, PolicyManager_Pretrain, PolicyManager_DownstreamRL, PolicyManager_DMPBaselines
+from PolicyManagers import PolicyManager_Joint, PolicyManager_Pretrain, PolicyManager_DownstreamRL, PolicyManager_DMPBaselines, PolicyManager_MemoryDownstreamRL
 import TestClass
 
 class Master():
