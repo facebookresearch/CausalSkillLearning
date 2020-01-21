50e3c4f82290ce8887c93577cee1153e42ff95dd
tanmayshankar <tanmayshankar@github.com>
2020-01-21 00:53:26 -0500

    Making sure repo was in right state

--- Experiments/Master.py
+++ Experiments/Master.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from headers import *
 import DataLoaders, MIME_DataLoader, Roboturk_DataLoader
-from PolicyManagers import PolicyManager_Joint, PolicyManager_Pretrain, PolicyManager_DownstreamRL, PolicyManager_DMPBaselines, PolicyManager_MemoryDownstreamRL
+from PolicyManagers import PolicyManager_Joint, PolicyManager_Pretrain, PolicyManager_DownstreamRL, PolicyManager_DMPBaselines, PolicyManager_BaselineRL
 import TestClass
 
 class Master():
