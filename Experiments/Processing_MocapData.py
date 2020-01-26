891a72e51af1c5b464ccc8614bc850c8090a29d5
tanmayshankar <tanmayshankar@github.com>
2020-01-26 00:44:38 -0500

    Adding mocap processing code.

--- Experiments/Processing_MocapData.py
+++ Experiments/Processing_MocapData.py
@@ -3,51 +3,34 @@ import mocap_processing
 from mocap_processing.motion.pfnn import Animation, BVH
 from mocap_processing.motion.pfnn import Animation, BVH
 
-filename = "/checkpoint/dgopinath/amass/CMU/01/01_01_poses.bvh"
-
-# def load_animation(bvh_filename):
-#     animation, joint_names, time_per_frame = BVH.load(bvh_filename)
-#     joint_parents = animation.parents
-#     global_positions = Animation.positions_global(animation)
-#     return global_positions, joint_parents, time_per_frame
-
+# Define function that loads global and local posiitons, and the rotations from a datafile.
 def load_animation_data(bvh_filename):
     animation, joint_names, time_per_frame = BVH.load(bvh_filename)
     global_positions = Animation.positions_global(animation)
     # return global_positions, joint_parents, time_per_frame
     return global_positions, animation.positions, animation.rotations
 
-# load_animation(filename)
-# load_animation(filename)[0]
-# load_animation(filename)[0].shape
-# load_animation(filename)[1]
-# load_animation(filename)[2]
-
-# 1/load_animation(filename)[2]
-
-# animation, joint_names, time_per_frame = BVH.load(filename)
-
-# animation
-# animation.positions
-# animation.rotations
-# animation.rotations.shape
-# animation.position.shape
-# animation.positions
-# animation.positions.shape
-# animation.rotations
-# animation.rotations[0]
-# animation.rotations[0][0]
-# animation.rotations[0][1]
-# get_ipython().run_line_magic('pwd', '')
-
+# Set directory. 
 directory = "/checkpoint/dgopinath/amass/CMU"
-filelist = glob.glob("*/*.bvh")
+save_directory = "/checkpoint/tanmayshankar/Mocap"
+# Get file list. 
+filelist = glob.glob(os.path.join(directory,"*/*.bvh"))
+
+demo_list = []
 
 for i in range(len(filelist)):
+	# Get filename. 
 	filename = os.path.join(directory, filelist[i])
+	# Actually load file. 
 	global_positions, local_positions, local_rotations = load_animation_data(filename)
 
+	# Create data element object.
 	data_element = {}
 	data_element['global_positions'] = global_positions
 	data_element['local_positions'] = global_positions
 	data_element['local_rotations'] = global_positions
+
+	demo_list.append(data_element)
+
+demo_array = np.array(demo_list)
+np.save(os.path.join(save_directory,"Demo_Array.npy"),demo_array)
