c2cc4018782232a2281bf65b61ae4942ebbebb35
tanmayshankar <tanmayshankar@github.com>
2020-01-29 00:28:53 -0500

    changing camera

--- Experiments/Visualizers.py
+++ Experiments/Visualizers.py
@@ -201,7 +201,7 @@ class MocapVisualizer():
 		# 						vup=np.array([0.0, 0.0, 1.0]), 
 		# 						fov=45.0)
 
-		self.cam_cur = MocapVisualizationUtils.camera.Camera(pos=np.array([3.0, 0.0, 2.0]),
+		self.cam_cur = MocapVisualizationUtils.camera.Camera(pos=np.array([4.5, 0.0, 2.0]),
 								origin=np.array([0.0, 0.0, 0.0]), 
 								vup=np.array([0.0, 0.0, 1.0]), 
 								fov=45.0)
