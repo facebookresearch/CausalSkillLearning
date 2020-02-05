4800eafb707f169cb278ffcc32fc73c10a7f1cbd
tanmayshankar <tanmayshankar@github.com>
2020-02-05 15:16:52 -0500

    Handle b prior latent policy in downstream RL

--- Experiments/PolicyManagers.py
+++ Experiments/PolicyManagers.py
@@ -2574,7 +2574,11 @@ class PolicyManager_DownstreamRL(PolicyManager_BaselineRL):
 		# Not sure if there's a better way to inherit - unless we inherit from both classes.
 		self.policy_network = ContinuousPolicyNetwork(self.input_size, self.args.hidden_size, self.output_size, self.args, self.args.number_layers).cuda()				
 		self.critic_network = CriticNetwork(self.input_size+self.conditional_info_size, self.args.hidden_size, 1, self.args, self.args.number_layers).cuda()
-		self.latent_policy = ContinuousLatentPolicyNetwork(self.input_size+self.conditional_info_size, self.args.hidden_size, self.args, self.args.number_layers).cuda()
+
+		if self.args.constrained_b_prior:
+			self.latent_policy = ContinuousLatentPolicyNetwork_ConstrainedBPrior(self.input_size+self.conditional_info_size, self.args.hidden_size, self.args, self.args.number_layers).cuda()
+		else:
+			self.latent_policy = ContinuousLatentPolicyNetwork(self.input_size+self.conditional_info_size, self.args.hidden_size, self.args, self.args.number_layers).cuda()
 
 	def create_training_ops(self):
 		
