bc8364c1ede8e6158837572075b7e6cc001d8804
tanmayshankar <tanmayshankar@github.com>
2020-08-05 12:29:03 -0400

    Adding License header.

--- DataGenerator/DirectedContinuousTrajs.py
+++ DataGenerator/DirectedContinuousTrajs.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Copyright (c) Facebook, Inc. and its affiliates.
 # All rights reserved.
 
--- DataLoaders/RoboturkeExp.py
+++ DataLoaders/RoboturkeExp.py
@@ -1,3 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 """
 A convenience script to playback random demonstrations from
 a set of demonstrations stored in a hdf5 file.
